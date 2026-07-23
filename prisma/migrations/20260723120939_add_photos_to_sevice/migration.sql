7fa9a0297b744746bdaf2e22fa6d4f009619b97b
copperlamb78 <nascimento.gabriel2004@gmail.com>
2026-07-23 09:29:42 -0300

    feat: adiciona campos de foto e logo ao Provider e atualiza DTO

---
+++ prisma/migrations/20260723120939_add_photos_to_sevice/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "Provider" ADD COLUMN     "bannerPhoto" TEXT,
+ADD COLUMN     "logoPhoto" TEXT;
--- src/modules/providers/dto/providers-update.dto.ts
+++ src/modules/providers/dto/providers-update.dto.ts
@@ -1,4 +1,25 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateProviderDto } from './providers-create.dto';
+import { IsOptional, IsUrl } from 'class-validator';
 
-export class UpdateProviderDto extends PartialType(CreateProviderDto) {}
+export class UpdateProviderDto extends PartialType(CreateProviderDto) {
+  @ApiProperty({
+    example:
+      'https://res.cloudinary.com/sinalizego/image/upload/v1700000000/sinalizego/providerId/banner/public_id.jpg',
+    description: 'URL do banner do negócio',
+    required: false,
+  })
+  @IsOptional()
+  @IsUrl({}, { message: 'A URL do banner é inválida' })
+  banner?: string;
+
+  @ApiProperty({
+    example:
+      'https://res.cloudinary.com/sinalizego/image/upload/v1700000000/sinalizego/providerId/logo/public_id.jpg',
+    description: 'URL da logo do negócio',
+    required: false,
+  })
+  @IsOptional()
+  @IsUrl({}, { message: 'A URL da logo é inválida' })
+  logo?: string;
+}
