ab28c0a57525b8379821e0c63997622348a14ffe
copperlamb78 <nascimento.gabriel2004@gmail.com>
2026-07-31 19:31:01 -0300

    fix: registra AsaasModule no UsersModule para injetar AsaasService

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { AsaasModule } from 'src/asaas/asaas.module';
 
 @Module({
+  imports: [AsaasModule],
   providers: [UsersService],
-  controllers: [UsersController]
+  controllers: [UsersController],
 })
 export class UsersModule {}
