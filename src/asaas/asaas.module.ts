fb88e6ac988b2103809c2dd8d2dac2ee20574c54
copperlamb78 <nascimento.gabriel2004@gmail.com>
2026-07-31 19:32:07 -0300

    fix: corrige injecao de CalculateTax e AsaasService nos modulos Asaas e Transactions

--- src/asaas/asaas.module.ts
+++ src/asaas/asaas.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AsaasService } from './asaas.service';
 import { AsaasController } from './asaas.controller';
+import { CalculateTax } from 'src/helpers/calculate-tax.helper';
 
 @Module({
-  providers: [AsaasService],
+  providers: [AsaasService, CalculateTax],
   exports: [AsaasService],
   controllers: [AsaasController],
 })
--- src/modules/transactions/transactions.module.ts
+++ src/modules/transactions/transactions.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { TransactionsController } from './transactions.controller';
 import { AsaasService } from 'src/asaas/asaas.service';
+import { CalculateTax } from 'src/helpers/calculate-tax.helper';
 
 @Module({
-  providers: [TransactionsService],
-  imports: [AsaasService],
+  providers: [TransactionsService, AsaasService, CalculateTax],
   controllers: [TransactionsController],
 })
 export class TransactionsModule {}
