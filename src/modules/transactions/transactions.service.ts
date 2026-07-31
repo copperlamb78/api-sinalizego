d979df19994e446abaf157a1ebea7817836cc427
copperlamb78 <nascimento.gabriel2004@gmail.com>
2026-07-31 19:33:33 -0300

    feat: valida existencia de cliente, agendamento e barbeiro em TransactionsService

--- src/modules/transactions/transactions.service.ts
+++ src/modules/transactions/transactions.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { AsaasService } from 'src/asaas/asaas.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreatePixTransactionDto } from './dto/transactions-create.dto';
@@ -12,15 +16,34 @@ export class TransactionsService {
 
   async createPixTransaction(data: CreatePixTransactionDto) {
     const existingCustomer = await this.prisma.user.findUnique({
-      where: { id: data.customerId, asaasCustomerId: data.asaasCustomerId },
+      where: { id: data.customerId },
+      select: { id: true, asaasCustomerId: true },
     });
     if (!existingCustomer) {
       throw new NotFoundException('Cliente não encontrado');
+    }
+    if (!existingCustomer.asaasCustomerId) {
+      throw new BadRequestException(
+        'Cliente não possui cadastro financeiro na Asaas',
+      );
+    }
+
+    const appointment = await this.prisma.appointment.findUnique({
+      where: { id: data.appointmentId },
+    });
+    if (!appointment) {
+      throw new NotFoundException('Agendamento não encontrado.');
     }
 
     const existingBarber = await this.prisma.financialProfile.findUnique({
       where: { walletId: data.barberWalletId },
     });
+
+    if (!existingBarber) {
+      throw new NotFoundException(
+        'Barbearia não encontrada ou sem carteira cadastrada.',
+      );
+    }
 
     const pixData = await this.asaas.createPixChargeWithSplit(
       data.asaasCustomerId,
