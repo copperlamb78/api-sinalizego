56446325cf9c9185a98cc18d269447dee902faf0
copperlamb78 <nascimento.gabriel2004@gmail.com>
2026-09-06 10:42:16 -0300

    feat(webhook): importa FoundersModule e ReferralsModule em WebhooksModule

--- src/asaas/webhook-asaas/webhooks.module.ts
+++ src/asaas/webhook-asaas/webhooks.module.ts
@@ -4,9 +4,17 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 import { WebhooksService } from './webhooks.service';
 import { AsaasModule } from '../asaas.module';
 import { MailModule } from 'src/modules/mail/mail.module';
+import { FoundersModule } from 'src/modules/founders/founders.module';
+import { ReferralsModule } from 'src/modules/referrals/referrals.module';
 
 @Module({
-  imports: [PrismaModule, AsaasModule, MailModule],
+  imports: [
+    PrismaModule,
+    AsaasModule,
+    MailModule,
+    FoundersModule,
+    ReferralsModule,
+  ],
   controllers: [WebhooksController],
   providers: [WebhooksService],
 })
