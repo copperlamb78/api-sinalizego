b325990fd4d5c685c3ab0c03882e742d26b25645
copperlamb78 <nascimento.gabriel2004@gmail.com>
2026-09-06 10:41:31 -0300

    feat(empresa): importa ReferralsModule em CompanyModule

--- src/modules/company/company.module.ts
+++ src/modules/company/company.module.ts
@@ -4,9 +4,10 @@ import { CompanyController } from './company.controller';
 import { SlugHelper } from './helpers/create-slug.helper';
 import { AuthModule } from '../auth/auth.module';
 import { AsaasModule } from 'src/asaas/asaas.module';
+import { ReferralsModule } from '../referrals/referrals.module';
 
 @Module({
-  imports: [AuthModule, AsaasModule],
+  imports: [AuthModule, AsaasModule, ReferralsModule],
   providers: [CompanyService, SlugHelper],
   controllers: [CompanyController],
   exports: [CompanyService],
