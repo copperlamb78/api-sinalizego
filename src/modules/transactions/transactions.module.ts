00c3e1c98a2baf72a105e0f6d0463828f5e5e6e9
copperlamb78 <nascimento.gabriel2004@gmail.com>
2026-09-06 10:40:54 -0300

    feat(transacoes): importa FeesModule em TransactionsModule

--- src/modules/transactions/transactions.module.ts
+++ src/modules/transactions/transactions.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { TransactionsController } from './transactions.controller';
 import { AsaasModule } from 'src/asaas/asaas.module';
+import { FeesModule } from '../fees/fees.module';
 
 @Module({
-  imports: [AsaasModule],
+  imports: [AsaasModule, FeesModule],
   providers: [TransactionsService],
   controllers: [TransactionsController],
   exports: [TransactionsService],
